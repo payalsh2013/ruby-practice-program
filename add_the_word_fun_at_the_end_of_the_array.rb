f4bbbb3926090a2afea6a497608cc99e8eec8ca8
payal <payalsh2013@gmail.com>
2021-06-10 19:24:13 +0530

    update program

--- add_the_word_fun_at_the_end_of_the_array.rb
+++ add_the_word_fun_at_the_end_of_the_array.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
 
+# add last employee name into  array list
 class AddArray
-  def funify()
-employees = Array["hello", "mona"]
-employees[2] = "payal"
-puts employees
+  def funify
+    employees = Array['hello', 'mona']
+    employees[2] = 'payal'
+    puts employees
   end
 end
 object = AddArray.new
--- base_ball_player.rb
+++ base_ball_player.rb
@@ -1,19 +1,16 @@
+# frozen_string_literal: true
 
-=begin
-Create a BaseballPlayer class that is initialized with hits, walks, and at_bats.
-Add a batting_average() method that returns hits divided by at_bats as a floating point number.
-Add an on_base_percentage() method that returns (hits + walks) divided by at_bats as a floating point number.
-Demonstrate how the batting_average() and on_base_percentage() methods can be used.
-=end
+# base ball player
 class BaseballPlayer
-@@hits, @@walks, @@at_bats = 40, 40, 4
-def batting_average
-puts @@hits/@@at_bats.to_f
-end
-def on_base_percentage
-puts (@@hits + @@walks)/@@at_bats.to_f
-
-end
+  @hits = 40
+  @walks = 40
+  @at_bats = 4
+  def batting_average
+    puts @@hits / @@at_bats.to_f
+  end
+  def on_base_percentage
+    puts (@@hits + @@walks) / @@at_bats.to_f
+  end
 end
 object = BaseballPlayer.new
 object.batting_average
