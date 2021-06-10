2430360f40754156bdbf32fbf540a85ae200767d
payal <payalsh2013@gmail.com>
2021-06-10 20:08:48 +0530

    fix rubocop error

--- convert_word_into_string.rb
+++ convert_word_into_string.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# convert word into string
 class ConvertString
-def content
-  name =["waters",+ "too", "polluted", "with", "germs"]
-  puts "#{name[0]} #{name[1]} #{name[2]} #{name[3]} #{name[4]}"
-end
+  def content
+    name = ['waters', + 'too', 'polluted', 'with', 'germs']
+    puts "#{name[0]} #{name[1]} #{name[2]} #{name[3]} #{name[4]}"
+  end
 end
 object = ConvertString.new
 object.content
